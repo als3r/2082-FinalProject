64c160a5607a78541ea1c3149c5b8bef0d3b15a1
Alex <sergeyev.alex@gmail.com>
2019-11-13 17:12:36 -0600

    add genre and movie time classes

---
+++ src/edu/century/finalproject/Genre.java
@@ -0,0 +1,18 @@
+package edu.century.finalproject;
+
+public class Genre {
+	
+	String name;
+	
+	public Genre(String genre) {
+		name = genre;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/edu/century/finalproject/MovieTime.java
@@ -0,0 +1,18 @@
+package edu.century.finalproject;
+
+public class MovieTime {
+
+	String time;
+	
+	public MovieTime(String time) {
+		this.time = time;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+}
